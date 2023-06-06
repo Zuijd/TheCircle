96761a4eae1d58fa34a07dfe1c2ba8af546dbbc5
Jens <25007405+JensdeVlaming@users.noreply.github.com>
2023-06-06 10:35:58 +0200

    added empty Stream controller and index view

---
+++ Portal/Controllers/StreamController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Portal.Models;
+
+namespace Portal.Controllers;
+
+public class StreamController : Controller
+{
+    private readonly ILogger<StreamController> _logger;
+
+    public StreamController(ILogger<StreamController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
