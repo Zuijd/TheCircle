e815ce6f1f31ef8cbe7d555897af9803a82902cf
GuusvD <g.vandamme@student.avans.nl>
2023-06-10 20:09:02 +0200

    Move user validation and errors to UserService, create MultipleExceptions and KeyException

--- Portal/Controllers/UserController.cs
+++ Portal/Controllers/UserController.cs
@@ -1,19 +1,12 @@
-using Microsoft.AspNetCore.Identity;
-using System.Text.RegularExpressions;
-
 namespace Portal.Controllers
 {
     public class UserController : Controller
     {
         private readonly IUserService _userService;
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IUserService userService)
+        public UserController(IUserService userService)
         {
             _userService = userService;
-            _userManager = userManager;
-            _signInManager = signInManager;
         }
 
         [PreventAccessFilter]
@@ -28,15 +21,15 @@ namespace Portal.Controllers
                 {
                     var user = await _userService.LoginUserAsync(loginViewModel.Username!, loginViewModel.Password!);
 
-                    if(user)
+                    if (user)
                     {
                         return RedirectToAction("Index", "Home");
                     }
                 }
             }
-            catch (Exception e)
+            catch (KeyException e)
             {
-                ModelState.AddModelError(nameof(loginViewModel.Password), e.Message);
+                ModelState.AddModelError(e.Key, e.Message);
             }
 
             return View(loginViewModel);
@@ -48,56 +41,26 @@ namespace Portal.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
-            if (registerViewModel.Username != null)
+            try
             {
-                if(registerViewModel.Username.Length < 6)
-                    ModelState.AddModelError("Username", "The username should be a at least 6 characters long!");
-                
-                else
+                if (ModelState.IsValid)
                 {
-                    var existingUser = await _userManager.FindByNameAsync(registerViewModel.Username);
+                    var result = await _userService.RegisterUserAsync(registerViewModel.Username!, registerViewModel.EmailAddress!, registerViewModel.Password!);
 
-                    if (existingUser != null)
-                    ModelState.AddModelError("Username", "The username is already associated with an account!");
+                    if (result)
+                    {
+                        await _userService.LoginUserAsync(registerViewModel.Username!, registerViewModel.Password!);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            } catch (MultipleExceptions e)
+            {
+                foreach (var innerExc in e.InnerExceptions)
+                {
+                    ModelState.AddModelError((((KeyException) innerExc).Key), (((KeyException) innerExc).Message));
                 }
             }
 
-            if (registerViewModel.Password != null)
-                if (!PasswordValidation(registerViewModel.Password))
-                   ModelState.AddModelError("Password", "The password should be at least 8 characters long and contrain at least 1 uppercase letter and 1 number!");
-
-            if (registerViewModel.EmailAddress != null)
-            {
-                if (!EmailValidation(registerViewModel.EmailAddress))
-                {
-                    ModelState.AddModelError("EmailAddress", "The emailaddress is not valid!");
-                }
-                else
-                {
-                    var existingUser = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
-
-                    if (existingUser != null)
-                    ModelState.AddModelError("EmailAddress", "The emailaddress is already associated with an account!");
-                }
-            }
-
-            if (ModelState.IsValid)
-            {
-                var user = new IdentityUser
-                {
-                    UserName = registerViewModel.Username,
-                    Email = registerViewModel.EmailAddress
-                };
-
-                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
-
-                if (result.Succeeded)
-                {
-                    await _signInManager.PasswordSignInAsync(user, registerViewModel.Password, false, false);
-                    return RedirectToAction("Index", "Home");
-                }
-                return View(registerViewModel);
-            }
             return View(registerViewModel);
         }
 
@@ -112,28 +75,12 @@ namespace Portal.Controllers
                     var user = await _userService.SignUserOutAsync();
                 }
             }
-            catch (Exception e)
+            catch (KeyException e)
             {
-                ModelState.AddModelError(e.Message, e.Message);
+                ModelState.AddModelError(e.Key, e.Message);
             }
 
             return RedirectToAction("Index", "Home");
-        }
-
-        public bool PasswordValidation(string password)
-        {
-            var hasNumber = new Regex(@"[0-9]+");
-            var hasUpperChar = new Regex(@"[A-Z]+");
-            var hasMinimum8Chars = new Regex(@".{8,}");
-
-            return hasNumber.IsMatch(password) && hasUpperChar.IsMatch(password) && hasMinimum8Chars.IsMatch(password);
-        }
-
-        public bool EmailValidation(string email)
-        {
-            var mailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
-
-            return mailRegex.IsMatch(email);
         }
     }
 }
