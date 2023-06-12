7ba31d2d439d12c3053e6ab2314afa698eb7b9e2
zjwanzankana <zjwanzankana@gmail.com>
2023-06-13 01:09:52 +0200

    Added WatchCount and streamer
    * WatchCount and streamer are functional / WatchCount and viewStream need work because of the possibility for multiple streams

--- Portal/Controllers/HomeController.cs
+++ Portal/Controllers/HomeController.cs
@@ -2,25 +2,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Portal.Models;
 
-namespace Portal.Controllers;
-
-public class HomeController : Controller
+namespace Portal.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Stream()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
--- Portal/wwwroot/js/stream.js
+++ Portal/wwwroot/js/stream.js
@@ -1,3 +1,23 @@
+$(document).ready(function () {
+    $('#startStreamBtn').click(function () {
+        var button = $(this);
+        var videoBox = $('#videoBox');
+        var circleImage = $('#circleImage');
+
+        if (button.hasClass('btn-success')) {
+            button.removeClass('btn-success').addClass('btn-danger').text('Stop Streaming');
+            videoBox.show();
+            circleImage.hide();
+            startStreaming();
+        } else {
+            button.removeClass('btn-danger').addClass('btn-success').text('Start Streaming');
+            videoBox.hide();
+            circleImage.show();
+            stopStreaming();
+        }
+    });
+});
+
 "use strict";
 
 let mediaRecorder;
@@ -133,3 +153,4 @@ function updateWatcherCountUI() {
     const watcherCountElement = document.getElementById('watcherCount');
     watcherCountElement.textContent = (watcherCount-1).toString();
 }
+
