364d8fb9a6c83ba3dc640f746ebc9eac9fc2c09c
KasperVanDenEnden <kaspervde1996@hotmail.com>
2023-06-13 11:31:45 +0200

    added Stream-live-break classes

---
+++ Domain/Stream.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Stream
+    {
+        [Required]
+        public int Id {  get; set; }
+        public List<Live>? LiveList { get; set; }
+        public List<Break>? BreakList { get; set; }
+        public float Satoshi { get; set; }
+        [Required]
+        public bool? Live { get; set; }
+    }
+
+    public class Live {
+        [Required]
+        public int Id { get; set; }
+        public DateTime? DateTimeStart { get; set; }
+        public DateTime? DateTimeEnd { get; set; }
+        public TimeSpan? Duration { get; set; }
+    }
+
+    public class Break
+    {
+        [Required]
+        public int Id { get; set; }
+        public DateTime? DateTimeStart { get; set; }
+        public DateTime? DateTimeEnd { get; set;}
+        public TimeSpan? Duration { get; set; }
+    }
+}
