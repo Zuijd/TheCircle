1b412f0c62fd4f8f3b7537a6b6c4d7be5f6cf22f
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2023-06-13 13:57:47 +0200

    Stream chat with username

--- Portal/wwwroot/js/stream.js
+++ Portal/wwwroot/js/stream.js
@@ -1,27 +1,26 @@
-"use strict";
+//"use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+//var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-document.getElementById("sendButton").disabled = true;
+//document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (message, user) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    //li.textContent = `${user} says ${message}`;
-    li.textContent = `${user} : ${message}`;
-});
+//connection.on("ReceiveMessage", function (user, message) {
+//    var li = document.createElement("li");
+//    document.getElementById("messagesList").appendChild(li);
+//    li.textContent = `${user} : ${message}`;
+//});
 
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
+//connection.start().then(function () {
+//    document.getElementById("sendButton").disabled = false;
+//}).catch(function (err) {
+//    return console.error(err.toString());
+//});
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
-});
+//document.getElementById("sendButton").addEventListener("click", function (event) {
+//    //var user = document.getElementById("userInput").value;
+//    var message = document.getElementById("messageInput").value;
+//    connection.invoke("SendMessage", user, message).catch(function (err) {
+//        return console.error(err.toString());
+//    });
+//    event.preventDefault();
+//});
