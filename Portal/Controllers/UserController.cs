7166295bcd29d75ed4d1e040acac3ccb409a71f1
Calvin <cl.kleijn@student.avans.nl>
2023-06-20 19:20:14 +0200

    Add back some accidentally removed code

--- Portal/Controllers/UserController.cs
+++ Portal/Controllers/UserController.cs
@@ -5,11 +5,13 @@
     {
         private readonly IUserService _userService;
         private readonly ILoggerService _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public UserController(IUserService userService, ILoggerService logger)
+        public UserController(IUserService userService, ILoggerService logger, IHttpContextAccessor httpContextAccessor)
         {
             _userService = userService;
             _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         [PreventAccess]
@@ -24,6 +26,7 @@
 
                     if (user)
                     {
+                        HttpContext.Session.SetString("Username", loginViewModel.Username!);
                         _logger.Log(loginViewModel.Username, $"{loginViewModel.Username} has logged in!");
                         
                         return RedirectToAction("Index", "Home");
@@ -79,6 +82,7 @@
                 {
                     _logger.Log(User.Identity!.Name!, $"{User.Identity!.Name!} has logged out");
                     var user = await _userService.SignUserOutAsync();
+                    HttpContext.Session.Remove("Username");
                 }
             }
             catch (KeyException e)
